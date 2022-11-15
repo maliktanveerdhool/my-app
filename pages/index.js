a562bf12d06de91f8683011d6d8c61d7f8ed5b8f
tanveerulhassan <maliktanveer2641@gmail.com>
2022-11-15 18:42:14 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome  <a href="https://nextjs.org">Malik Tanveer Dhool!</a>
         </h1>
 
         <p className={styles.description}>
@@ -68,4 +68,11 @@ export default function Home() {
       </footer>
     </div>
   )
+
+
+
+ 
 }
+
+
+
